1e0e501c3490161b1f48fb7663a26fc0e5d56ffe
Radhika-Sonar <sonarradhika1@gmail.com>
2026-01-19 14:47:33 +0530

    Update LoginAutomation.java

--- src/test/java/com/qa/Tests/LoginAutomation.java
+++ src/test/java/com/qa/Tests/LoginAutomation.java
@@ -50,7 +50,7 @@ public class LoginAutomation
 		   driver.findElement(By.xpath("//button[text()='Login']")).click();
 		   driver.findElement(By.linkText("View profile")).click();
 		   
-		   File file = new File("src/main/resources/Files/Radhika_Sonar_Data_Analyst_M.pdf");
+		   File file = new File("src/main/resources/Files/Radhika_Sonar_Data_Analyst_No..pdf");
 		   driver.findElement(By.xpath("//input[@id='attachCV']")).sendKeys(file.getAbsolutePath());
 		   
 		   
